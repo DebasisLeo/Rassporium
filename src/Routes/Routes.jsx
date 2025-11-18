c837780479aa183932a24c6d59f7de0edcff309e
debasis <joydebashis8@gmail.com>
2025-11-18 23:20:51 +0600

    routes added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,11 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
-// import Login from "../pages/Login/Login";
-// import SignUp from "../pages/Signup/SignUp";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/Signup/SignUp";
 import PrivateRoute from "./PrivateRoute";
-// import Dashboard from "../Layout/Dashboard";
-// import Cart from "../pages/Dashboard/cart/Cart";
+import Dashboard from "../Layout/Dashboard";
+import Cart from "../pages/Dashboard/cart/Cart";
 import NotFoundPage from "../pages/NotFoundPage";
 
 
@@ -29,24 +29,24 @@ export const router = createBrowserRouter([
         path: "order/:category",
         element: <PrivateRoute><Order /></PrivateRoute>
       },
-      // {
-      //   path: "login",
-      //   element: <Login />
-      // },
-      // {
-      //   path: "signup",
-      //   element: <SignUp />
-      // }
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "signup",
+        element: <SignUp />
+      }
     ]
   },
-  // {
-  //   path: "dashboard",
-  //   element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
-  //   children: [
-  //     {
-  //       path: "cart",
-  //       element: <Cart />
-  //     }
-  //   ]
-  // }
+  {
+    path: "dashboard",
+    element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
+    children: [
+      {
+        path: "cart",
+        element: <Cart />
+      }
+    ]
+  }
 ]);
